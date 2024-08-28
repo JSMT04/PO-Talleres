d0d1dfd63a1564d72646be8de6f15342b1f808e1
julianv2110 <jvalencia9902@gmail.com>
2024-08-27 22:47:23 -0500

    primera modificación

--- TALLER.java
+++ TALLER.java
@@ -2,6 +2,7 @@ package taller;
 public class TALLER {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        System.out.println("como estan!");
     }
     
 }
